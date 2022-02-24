a4879e370f701425177fea8cee02182f55f33c19
Henry-Roberto <henroberto@hotmail.com>
2022-02-24 12:41:10 -0300

    Atualizando projeto

--- src/Controller/WhatsAppController.js
+++ src/Controller/WhatsAppController.js
@@ -82,7 +82,7 @@ export class WhatsAppController {
 
 
             }).catch(err => {
-                console.log(err);
+                console.error(err);
             })
     }
 
@@ -194,25 +194,37 @@ export class WhatsAppController {
             display: 'flex'
         });
 
+        this.el.panelMessagesContainer.innerHTML = '';
+
         Message.getRef(this._contactActive.chatId).orderBy('timeStamp')
             .onSnapshot(docs => {
-                this.el.panelMessagesContainer.innerHTML = '';
+                let scrollTop = this.el.panelMessagesContainer.scrollTop;
+                let scrollTopMax = this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight;
+                let autoScroll = (scrollTop >= scrollTopMax);
+
                 docs.forEach(doc => {
                     let data = doc.data();
                     data.id = doc.id;
 
-                    console.log(data.id);
                     if (!this.el.panelMessagesContainer.querySelector('#_' + data.id)) {
+
                         let message = new Message();
                         message.fromJSON(data);
 
                         let me = (data.from === this._user.email);
                         let view = message.getViewElement(me);
-                        console.log(view);
                         this.el.panelMessagesContainer.appendChild(view);
+
+
                     }
 
                 });
+
+                if (autoScroll) {
+                    this.el.panelMessagesContainer.scrollTop = this.el.panelMessagesContainer.scrollHeight - this.el.panelMessagesContainer.offsetHeight;
+                } else {
+                    this.el.panelMessagesContainer.scrollTop = scrollTop;
+                }
 
             });
 
@@ -433,7 +445,6 @@ export class WhatsAppController {
 
         this.el.inputPhoto.on('change', e => {
 
-            console.log(this.el.inputPhoto.files);
 
             [...this.el.inputPhoto.files].forEach(file => {
 
